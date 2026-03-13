5fe7ed0ebfd5775d9a1786f8644d0028dc857c4a
Diego de Lima Lopes <diego.lima.lopes@hotmail.com.br>
2026-03-13 14:43:18 -0300

    Implementação do modelo de com a estrutura de arquitetura limpa em modulos

--- entrypoint/src/main/java/br/com/sdweb/agenda/entrypoint/controller/httmodel/EventResponse.java
+++ entrypoint/src/main/java/br/com/sdweb/agenda/entrypoint/controller/httmodel/EventResponse.java
@@ -2,9 +2,6 @@ package br.com.sdweb.agenda.entrypoint.controller.httmodel;
 
 import br.com.sdweb.agenda.entrypoint.controller.httmodel.enums.RecorrencyEnum;
 import br.com.sdweb.agenda.entrypoint.controller.httmodel.enums.TypeEventEnum;
-import com.fasterxml.jackson.annotation.JsonInclude;
-import jakarta.validation.constraints.NotNull;
-import org.springframework.format.annotation.DateTimeFormat;
 
 import java.time.LocalDateTime;
 
